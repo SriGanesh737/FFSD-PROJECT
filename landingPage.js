82c1f5085a8c72d2ccdfd8bd69d14ca5580d4c80
Sri Ganesh <sriganeshiiits@gmail.com>
2023-03-12 21:17:24 +0530

    fixed a bug to open correctly when clicked on a topic

--- landingPage.js
+++ landingPage.js
@@ -1,8 +1,7 @@
 $('.card').click(function (e) {
     let currentUrl = window.location.origin;
 
-   let redirectFile = "Health.html";
-
-//    window.location.href = currentUrl + "/" + redirectFile;
-   window.open("./" + redirectFile, "_blank");
+   let redirectFile = "./Health.html";
+   window.location.href = redirectFile;
+   
 });
