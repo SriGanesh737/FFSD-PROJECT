30beda5d4725200d6cc177f3df7248f2b7f5f8e0
Sri Ganesh <sriganeshiiits@gmail.com>
2023-03-12 11:25:07 +0530

    added modal feature

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+let is_clicked = false;
 
+
+$('.article').hover(function () {
+
+    $(this).css('transform', 'scale(1.05)');
+
+    }, function () {
+
+    $(this).css('transform', 'scale(1)');
+    }
+);
+
+$('.article').click(function (e) {
+    e.preventDefault();
+
+    let clickedSource=e.currentTarget.children[0].children[0].getAttribute('src');
+
+
+    $('.pop-image').attr('src', clickedSource);
+
+    $('.modal-image').css('display', 'block');
+
+    //scroll to the top of the page
+
+    $(window).scrollTop(0);
+    $('body').css('overflow', 'hidden');
+    is_clicked = true;
+});
+
+$('.close-btn').click(function (e) {
+    // e.preventDefault();
+    if(is_clicked)
+        $('.modal-image').css('display', 'none');
+    $('body').css('overflow', 'auto');
+    is_clciked = false;
+
+});
